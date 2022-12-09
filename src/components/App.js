296207242551da73514e1483d98d2cc12b4deb05
BastienVienet <bastien.vienet@outlook.com>
2022-12-09 17:46:27 +0100

    Adding todos

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import logo from '../assets/logo.svg';
-import '../styles/App.css';
+import React, {useState} from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 100vh;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+
+    const [toDoList, setToDoList] = useState([])
+    const [input, setInput] = useState("")
+
+    const addTodo = (todo) => {
+        const newTodo = {
+            id: Math.floor(Math.random() * 100),
+            todo : todo
+        };
+        // Add the todo to the list
+        setToDoList(([...toDoList, newTodo]));
+        // Clear input box
+        setInput('');
+    }
+
+    return (
+        <Container>
+            <h1>To do list</h1>
+            <input type="text"
+                   value={input}
+                   onChange={(event) => setInput(event.target.value)}
+            />
+            <button onClick={() => addTodo(input)}>Add</button>
+        </Container>
+    );
 }
 
 export default App;
