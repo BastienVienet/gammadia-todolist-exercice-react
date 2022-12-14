4604843ee6a91d4d1aba7c449f8143f7558865c7
BastienVienet <bastien.vienet@outlook.com>
2022-12-14 16:23:30 +0100

    Line-through when checkbox is checked

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,12 +1,18 @@
 import { MdDelete } from 'react-icons/md'
+import styled from "styled-components";
+
+const ToggleLi = styled.li`
+${(props) => props.checked ? "text-decoration-line: line-through;" : ""}
+`
 
 export const ToDoItem = ({todo, onDelete, onCheckToDo}) => {
-    return <li>
-        <div>{todo.text}</div>
-        <button onClick={() => onDelete(todo.id)}><MdDelete></MdDelete></button>
+    return <ToggleLi checked={todo.completed}>
         <input
             type="checkbox"
+            checked={todo.completed}
             onChange={() => onCheckToDo(todo.id)}
         ></input>
-    </li>
+        <div>{todo.text}</div>
+        <button onClick={() => onDelete(todo.id)}><MdDelete></MdDelete></button>
+    </ToggleLi>
 }
